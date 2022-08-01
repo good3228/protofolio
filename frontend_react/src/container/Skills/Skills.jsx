a06dc55f5efa5e6ba902fc4817f128dfcf059385
good3228 <jim19980815@gmail.com>
2022-08-01 14:04:16 +0800

    footer section

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -16,7 +16,7 @@ const Skills = () => {
 
     client.fetch(query).then((data) => {
       setExperiences(data);
-      console.log(data);
+
     });
 
     client.fetch(skillsQuery).then((data) => {
